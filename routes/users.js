9e7bf4cd420ca74bd329c7136b1546b5eac3607e
thanhanDemon2003 <iamdemon.dev@gmail.com>
2023-10-31 22:56:48 +0700

    1

--- routes/users.js
+++ routes/users.js
@@ -24,7 +24,7 @@ router.post('/send-sms', (req, res) => {
       }
     }, (err, httpRes, body) => {
       if (err) {
-        return res.status(500).send('Lỗi gửi tin nhắn', err);
+        return res.status(500).send('Lỗi gửi tin nhắn', err, httpRes, body);
       }
       
       res.send('Tin nhắn đã được gửi');  
