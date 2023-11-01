696a8aedf037bdd7184315a504b05c32be53fa7d
thanhanDemon2003 <iamdemon.dev@gmail.com>
2023-11-01 17:32:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-const cors = require('cors');
+// const cors = require('cors');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
 const validate = require('./validate/Jwt');
-const allowedOrigins = ['https://gamedatn.andemongame.tech','https://thanhan.andemongame.tech', 'http://localhost:3000'];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
+// const allowedOrigins = ['https://gamedatn.andemongame.tech','https://thanhan.andemongame.tech', 'http://localhost:3000'];
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+// };
 var indexRouter = require('./routes/index');
 var cpanelRouter = require('./routes/cpanel');
 var usersRouter = require('./routes/users');
@@ -36,7 +36,7 @@ app.use('/', indexRouter)
 app.use('/admin',validate.authenticateJWT, cpanelRouter);
 app.use('/users', usersRouter);
 app.use('/games', gameRouter);
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 mongoose.connect('mongodb+srv://devnguyenthanhan26:123anan@cluster0.mmt4ppq.mongodb.net/game', {
   useNewUrlParser: true,
